70fae0572f9e820210f76eed046b2033d79844b3
Yiyang Xu <yxu@lab2r2s5.42abudhabi.ae>
2022-08-08 00:16:04 +0400

    C00 ex08

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -12,26 +12,53 @@
 
 #include <unistd.h>
 
-void ascend(char cb, int n, int dig)
+void print_comb(char *comb, int n)
 {
-   	
+	int	i;
 
-void ft_print_combn(int n)
-{
-	char cb[n];
-	int dig = 1;
-
-	cb[0] = 0;
-	while (dig <= n-1)
+	i = 0;
+	while (i <= n)
 	{
-		if 
-		cb[dig] = cb[dig-1] + 1;
-		dig++;
-		// write(1, &cb, n-1);
+		write(1, &comb[i++], 1);
+	}
+	if (comb[0] < 57 - n) // ASCII value 57 as '9'
+		write(1, ", ", 2);
+}
+
+void iterate(char *comb, int dig, int max, int n)
+{
+	while (comb[dig] <= 58 - max)
+	{
+		if (dig < n)
+		{
+			comb[dig + 1] = comb[dig] + 1;
+			iterate(comb, dig + 1, max - 1, n);
+			comb[dig]++;
+		}
+		else if (dig == n)
+		{
+			print_comb(comb, n);
+			comb[dig]++;
+		}
 	}
 }
 
+void ft_print_combn(int n)
+{
+	char comb[n];
+	int dig;
+	int max;
+
+	if (n > 10)
+		return;
+
+	max = n;
+	comb[0] = '0';
+	dig = 0;
+	iterate(comb, dig, max, n - 1);
+}
+/*
 int	main(void)
 {
 	ft_print_combn(3);
-}
+}*/
