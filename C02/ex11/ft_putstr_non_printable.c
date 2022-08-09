aaf7aa6035f99485edc7bf51b4a0570fb8e081f7
Yiyang Xu <yxu@lab2r2s5.42abudhabi.ae>
2022-08-09 16:41:23 +0400

    C02 ex11

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -19,13 +19,20 @@ void	ft_putstr_non_printable(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 32 && str[i] >= 126)
+		if (str[i] >= 32 && str[i] <= 126)
 			write(1, &str[i], 1);
 		else
 		{
-			write(1, "0123456789abcdef"[str[i] / 16]);
-			write(1, "0123456789abcdef"[str[i] % 16]);
+			write(1, "\\", 1);
+			write(1, &"0123456789abcdef"[str[i] / 16], 1);
+			write(1, &"0123456789abcdef"[str[i] % 16], 1);
 		}
 		i++;
 	}
 }
+/*
+int main (void)
+{
+	ft_putstr_non_printable("Coucou\n tu vas bie\t\v\0extra");
+	ft_putstr_non_printable("");
+}*/
